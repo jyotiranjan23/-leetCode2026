d6fa7380f751d824fdcdc7be721f10d8134ff548
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-02-10 13:07:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (48.20%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -2,8 +2,8 @@ class Solution {
     public void nextPermutation(int[] nums) {
         // first find the break point.
         int pivot = -1;
-        int n=nums.length-1;
-        for(int i=n; i>0; i--){
+        int lastIndex=nums.length-1;
+        for(int i=lastIndex; i>0; i--){
             if(nums[i] > nums[i-1]){
                 pivot = i-1;
                 break;
@@ -11,11 +11,11 @@ class Solution {
         }
         //if pivot == -1 then reverse the entire array
         if(pivot == -1){
-            reverse(nums,0,n);
+            reverse(nums,0,lastIndex);
         }
         else{
         // find the next greater element from n to pivot+1 and swap with pivot.
-        for(int i=n; i>pivot; i--){
+        for(int i=lastIndex; i>pivot; i--){
             if(nums[i] > nums[pivot]){
                 int temp = nums[i];
                 nums[i] = nums[pivot];
@@ -24,7 +24,7 @@ class Solution {
             }
         }
         //reverse the array from pivot+1 index to n.
-        reverse(nums, pivot+1,n);
+        reverse(nums, pivot+1,lastIndex);
         }
     }
     public void reverse(int[] arr, int i, int j){
