5e71ea1d9f45b8cab5e1ff70be80db5b9a1704d7
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-02-10 21:51:21 +0530

    Time: 7 ms (63.63%) | Memory: 44.2 MB (79.75%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -5,7 +5,6 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int lastIndex=stringArray.length-1;
         for(int i=lastIndex; i>=0; i--){
-            if(stringArray[i] == " ")continue;
             sb.append(stringArray[i]);
             if(i!= 0)
             sb.append(" ");
