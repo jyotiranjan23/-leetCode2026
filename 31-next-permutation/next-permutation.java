21ff8fe242573f415c70c722db3331e7eaba85e9
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-02-10 13:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (48.20%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,33 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        // first find the break point.
         int pivot = -1;
-        int n = nums.length;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i] < nums[i+1]){
-                pivot = i;
+        int n=nums.length-1;
+        for(int i=n; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                pivot = i-1;
                 break;
             }
         }
+        //if pivot == -1 then reverse the entire array
         if(pivot == -1){
-            reverse(nums,0,n-1);
-            return;
+            reverse(nums,0,n);
         }
-        for(int i=n-1; i>pivot; i--){
+        else{
+        // find the next greater element from n to pivot+1 and swap with pivot.
+        for(int i=n; i>pivot; i--){
             if(nums[i] > nums[pivot]){
-                int temp = nums[pivot];
-                nums[pivot] = nums[i];
-                nums[i] = temp;
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
                 break;
             }
         }
-        reverse(nums,pivot+1,n-1);
-        
+        //reverse the array from pivot+1 index to n.
+        reverse(nums, pivot+1,n);
+        }
     }
-    public void reverse(int[]nums, int i, int j){
-        while(i<=j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+    public void reverse(int[] arr, int i, int j){
+        while(i <= j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
             i++;
             j--;
         }
