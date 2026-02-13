80686b690249ab1bd4de9d9f95838b508dfc33a2
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-02-13 11:56:15 +0530

    Time: 1 ms (60.40%) | Memory: 47 MB (96.23%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,41 +2,34 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean firstRow = false;
         boolean firstColumn = false;
-        //travers the 2d array and if you found 0 then mark 0 in it's   corresponding first row and first column.
+        //traverse the matrix from row0 and column0 and if you found 0 then make the corespending 0th row and 0th column as 0.
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[i].length; j++){
                 if(matrix[i][j] == 0){
                     if(i==0) firstRow = true;
                     if(j==0) firstColumn = true;
-                    matrix[i][0] = 0;
                     matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-
-        //travers the 2d array from column = 1 and row =1 and if the marker value is 0 then make it as 0.
-        for(int i=1; i< matrix.length; i++){
-            for(int j=1; j< matrix[i].length; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        //traverse the matrix from row1 to column1 if the marker is 0 then make it as 0
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[i].length; j++){
+                if(matrix[0][j] == 0 || matrix[i][0]==0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        //if first row contains 0 then make the entire first row as 0
         if(firstRow){
-            for(int i=0; i< matrix[0].length; i++){
+            for(int i=0; i<matrix[0].length; i++){
                 matrix[0][i] = 0;
             }
         }
-
-        //if first cplumn contains 0 then make the entire column as 0
         if(firstColumn){
-            for(int i=0; i< matrix.length; i++){
+            for(int i=0; i<matrix.length; i++){
                 matrix[i][0] = 0;
             }
         }
-
-        
-        
     }
 }
