11faaeab115ddacf6db78d118cdb45fdd755d36a
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-02-18 16:12:32 +0530

    Time: 22 ms (6.73%) | Memory: 51.8 MB (84.35%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int requiredFrequency = nums.length/3;
+        List<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            if(map.getOrDefault(nums[i], 0) > requiredFrequency){
+                result.add(nums[i]);
+            }
+        }
+        return result.stream().distinct().toList();
+        
+    }
+}
