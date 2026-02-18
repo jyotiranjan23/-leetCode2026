d644db771f31024587d72a291dafd0c802a339fb
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-02-18 17:19:04 +0530

    Time: 4 ms (57.96%) | Memory: 52.5 MB (63.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,16 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int requiredFrequency = nums.length/3;
-        List<Integer> result = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            if(entry.getValue() > requiredFrequency){
-                result.add(entry.getKey());
+        int majorityValue = nums.length/3;
+        Integer maj1 = -1;
+        Integer maj2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        for(int n: nums){
+            if(n == maj1){
+                count1++;
+            }
+            else if(n == maj2){
+                count2++;
+            }
+            else if(count1 == 0){
+                maj1 = n;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                maj2 = n;
+                count2 = 1;
+
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
+        List<Integer> result = new ArrayList<>();
+        int freq1 = 0;
+        int freq2 = 0;
+        for(int n: nums){
+            if(n == maj1) freq1++;
+            else if(n == maj2) freq2++;
+        }
+        if(freq1 > majorityValue) result.add(maj1);
+        if(freq2 > majorityValue) result.add(maj2);
+
         return result;
         
     }
