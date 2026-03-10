6d95757cc535d605fd93a1bb05950170c5dc3337
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-03-10 12:55:52 +0530

    Time: 5 ms (46.37%) | Memory: 94.5 MB (65.47%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -31,20 +31,17 @@ class Solution {
 
     }
     public ListNode reverse(ListNode head){
-        if(head == null || head.next == null)
-        return head;
-
-    ListNode prev = null;
-    ListNode curr = head;
-
-    while(curr != null){
-        ListNode next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
-    }
-
-    return prev;
+       if(head == null || head.next == null) return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            
+        }
+        return prev;
 
     }
 }
