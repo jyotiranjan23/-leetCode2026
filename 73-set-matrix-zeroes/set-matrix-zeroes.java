7f1e581efe6b62b9ff3d76d1e7cb2eb3b78040d1
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-03-26 11:38:34 +0530

    Time: 1 ms (70.94%) | Memory: 47.5 MB (38.34%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,21 +2,30 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean firstRow = false;
         boolean firstColumn = false;
-        //traverse the matrix from row0 and column0 and if you found 0 then make the corespending 0th row and 0th column as 0.
+
+        for(int i=0; i<matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                firstRow = true;
+                break;
+            } 
+        }
         for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[i].length; j++){
+            if(matrix[i][0] == 0){
+                firstColumn = true;
+                break;
+            }
+        }
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[i].length; j++){
                 if(matrix[i][j] == 0){
-                    if(i==0) firstRow = true;
-                    if(j==0) firstColumn = true;
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
             }
         }
-        //traverse the matrix from row1 to column1 if the marker is 0 then make it as 0
         for(int i=1; i<matrix.length; i++){
             for(int j=1; j<matrix[i].length; j++){
-                if(matrix[0][j] == 0 || matrix[i][0]==0){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
