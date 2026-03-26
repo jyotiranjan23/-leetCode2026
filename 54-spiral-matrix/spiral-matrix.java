d4eca8e537974a95b5e0146235816d8acd54f37e
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-03-26 07:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (54.40%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,29 +1,35 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int srow = 0,erow=matrix.length-1,scol=0,ecol=matrix[0].length-1;
-        List<Integer> al = new ArrayList<>();
-        while(srow <= erow && scol<=ecol){
+        List<Integer> result = new ArrayList<>();
+        int srow = 0;
+        int erow = matrix.length-1;
+        int scol = 0;
+        int ecol = matrix[0].length-1;
+        while(srow <= erow && scol <= ecol){
+            //top
             for(int i=scol; i<=ecol; i++){
-                al.add(matrix[srow][i]);
+                result.add(matrix[srow][i]);
             }
+            //right
             for(int i=srow+1; i<=erow; i++){
-                al.add(matrix[i][ecol]);
+                result.add(matrix[i][ecol]);
             }
-            for(int i=ecol-1; i>=scol;i--){
-                if(srow != erow){
-                    al.add(matrix[erow][i]);
-                }
+            //bottom
+            for(int i=ecol-1; i>= scol; i--){
+                if(srow == erow) break;
+                result.add(matrix[erow][i]);
             }
-            for(int i=erow-1; i>= srow+1; i--){
-                if(scol != ecol){
-                    al.add(matrix[i][scol]);
-                }
+            //left
+            for(int i=erow-1; i>srow; i--){
+                if(scol == ecol) break;
+                result.add(matrix[i][scol]);
             }
             srow++;
             erow--;
             scol++;
             ecol--;
+
         }
-        return al;
+        return result;
     }
 }
