5226062e30948aae7007d548daac3017d45df3d9
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-03-26 18:39:43 +0530

    Time: 1 ms (99.84%) | Memory: 55.7 MB (57.55%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int majorityElement = nums[0];
-        int frequency = 1;
+        int majorityElementFrequency = 1;
         for(int i=1; i<nums.length; i++){
             if(nums[i] == majorityElement){
-                frequency++;
+                majorityElementFrequency ++;
             }
-            else{
-                frequency--;
-                if(frequency == 0){
-                    majorityElement = nums[i];
-                    frequency++;
-                }
+            else {
+                majorityElementFrequency --;
+            if(majorityElementFrequency < 0){
+                majorityElement = nums[i];
+                majorityElementFrequency = 1;
+            }
+
             }
         }
         return majorityElement;
