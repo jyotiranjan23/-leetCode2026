3380c5da1e3b9d6c14a0076214eae451d15564ea
jyotiranjan23 <pandajyoti.2501@gmail.com>
2026-06-29 16:19:09 +0530

    Time: 1 ms (98.55%) | Memory: 47.6 MB (43.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,47 +1,49 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean rowContainsZero = false;
-        boolean columnContainsZero = false;
-        int m=matrix.length;
+        boolean firstRow = false;
+        boolean firstColumn = false;
+        int m = matrix.length;
         int n = matrix[0].length;
-        // check the first row contains 0 or not.
+        //check firstRow contains 0 or not 
         for(int i=0; i<n; i++){
             if(matrix[0][i] == 0){
-                rowContainsZero = true;
-                break;
+                firstRow = true;
             }
         }
-        // check the first column contains 0 or not.
+
+        //check firstColumn contains 0 or not 
         for(int i=0; i<m; i++){
             if(matrix[i][0] == 0){
-                columnContainsZero = true;
-                break;
+                firstColumn = true;
             }
         }
+
         for(int i=1; i<matrix.length; i++){
             for(int j=1; j<matrix[i].length; j++){
                 if(matrix[i][j] == 0){
-                    matrix[0][j] = 0;
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
+
         for(int i=1; i<matrix.length; i++){
             for(int j=1; j<matrix[i].length; j++){
-                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        if(rowContainsZero){
+
+        if(firstRow){
             for(int i=0; i<n; i++){
-            matrix[0][i] = 0;
+                matrix[0][i] = 0;
+            }
         }
-        }
-        if(columnContainsZero){
+        if(firstColumn){
             for(int i=0; i<m; i++){
-            matrix[i][0] = 0;
-        }
+                matrix[i][0] = 0;
+            }
         }
     }
 }
